c3f715020079c90511cead25f4e380fa4b8a3794
thekavak <hamzakavak@outlook.com>
2021-10-27 11:34:18 +0300

    added easyloadingDialog

--- challengeapp/lib/main.dart
+++ challengeapp/lib/main.dart
@@ -1,15 +1,39 @@
+import 'package:challengeapp/constant/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'view/home_view.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  configLoading();
+  runApp(const MyApp());
+}
+
+void configLoading() {
+  EasyLoading.instance
+    ..displayDuration = const Duration(milliseconds: 2000)
+    ..indicatorType = EasyLoadingIndicatorType.chasingDots
+    ..loadingStyle = EasyLoadingStyle.custom
+    ..indicatorSize = 50.0
+    ..radius = 10.0
+    ..progressColor = ThemeText.bottomBoxColor
+    ..backgroundColor = Colors.white
+    ..indicatorColor = ThemeText.bottomBoxColor
+    ..textColor = Colors.black
+    ..maskColor = Colors.black.withOpacity(0.3)
+    ..userInteractions = false
+    ..dismissOnTap = false;
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+        debugShowCheckedModeBanner: false,
+        builder: EasyLoading.init(),
         home: HomeView());
   }
 }
